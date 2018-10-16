bda387813199f293903c2511f88a5b98dc98ccfd
Emre Cürgül <pongo1999712@gmail.com>
2018-10-17 01:17:56 +0200

    Enter Key accepts input in ServerConnectForm

--- FiveMRcon/ServerConnectForm.Designer.cs
+++ FiveMRcon/ServerConnectForm.Designer.cs
@@ -105,6 +105,7 @@
 			// 
 			// ServerConnectForm
 			// 
+			this.AcceptButton = this.InputConnect;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(322, 97);
