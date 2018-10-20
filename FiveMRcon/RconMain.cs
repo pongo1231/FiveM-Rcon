d385d62543bdb4046da2e327b40b98aba773cf39
Emre Cürgül <pongo1999712@gmail.com>
2018-10-20 19:53:29 +0200

    Fix cmd args

--- FiveMRcon/RconMain.cs
+++ FiveMRcon/RconMain.cs
@@ -9,19 +9,19 @@ namespace FiveMRcon
 		static void Main()
 		{
 			string[] args = Environment.GetCommandLineArgs();
-			if (args.Length > 0)
-				InfoHolder.ServerIP = args[0];
 			if (args.Length > 1)
+				InfoHolder.ServerIP = args[1];
+			if (args.Length > 2)
 				try
 				{
-					InfoHolder.ServerPort = int.Parse(args[1]);
+					InfoHolder.ServerPort = int.Parse(args[2]);
 				}
 				catch (FormatException)
 				{
 					InfoHolder.ServerPort = 30120;
 				}
-			if (args.Length > 2)
-				InfoHolder.ServerPass = args[2];
+			if (args.Length > 3)
+				InfoHolder.ServerPass = args[3];
 
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
