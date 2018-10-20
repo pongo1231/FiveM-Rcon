fe65e55e67466ed10b44396154b8881219e91bf2
Emre Cürgül <pongo1999712@gmail.com>
2018-10-20 18:51:18 +0200

    Add support for commandline args

--- FiveMRcon/RconMain.cs
+++ FiveMRcon/RconMain.cs
@@ -8,6 +8,21 @@ namespace FiveMRcon
 		[STAThread]
 		static void Main()
 		{
+			string[] args = Environment.GetCommandLineArgs();
+			if (args.Length > 0)
+				InfoHolder.ServerIP = args[0];
+			if (args.Length > 1)
+				try
+				{
+					InfoHolder.ServerPort = int.Parse(args[1]);
+				}
+				catch (FormatException)
+				{
+					InfoHolder.ServerPort = 30120;
+				}
+			if (args.Length > 2)
+				InfoHolder.ServerPass = args[2];
+
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new RconForm());
